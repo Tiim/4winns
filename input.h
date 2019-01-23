8c7ff71f49d6342f8a541bacabeb080d87ec84ae
Tiim <tim.bachmann96@gmail.com>
2019-01-23 02:52:46 +0100

    Add more documentation

--- input.h
+++ input.h
@@ -1,10 +1,12 @@
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
+//define button connections
 #define BUTTON1 A3
 #define BUTTON2 A4
 #define BUTTON3 A5
 
+//define which button does what
 #define B_LEFT BUTTON1
 #define B_RIGHT BUTTON2
 #define B_ENTER BUTTON3
@@ -14,6 +16,10 @@
 #define RIGHT_PRESSED 1
 #define ENTER_PRESSED 2
 
+//returns -1 if move left
+//         0 if no move
+//         1 if move to the right
+//         2 if enter
 signed char getMove();
 
 #endif
--- logic.h
+++ logic.h
@@ -16,9 +16,15 @@ struct gamefield {
     chip field[FIELDS];
 };
 
+// calculates what happens if [player] inserts a coin in [collumn]
+// handles all neccesairy mutations to the [game] struct
+// returns chip position or -1 if no chip was inserted
 int insertChip(chip* player, struct gamefield* game, int collumn);
+// checks if the last move was a win. Only checks for the coin position [lastCoin]
 bool checkWin(struct gamefield* game, int lastCoin);
+// returns the row of a given field
 inline int getRow(int field);
+// returns the collumn of a given field
 inline int getCol(int field);
 
 #endif
